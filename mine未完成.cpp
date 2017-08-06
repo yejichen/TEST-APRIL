4c3797f15308d7b0ed359b4784e7f9d1ed18494d
yejichen <yejichen2@sina.cn>
2017-08-06 16:04:21 +0800

    Create mine未完成.cpp

---
+++ mine未完成.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int y[8]={-1,-1,-1,0,0,1,1,1};
+int x[8]={-1,0,1,-1,1,-1,0,1};
+int find(int y,int x)
+{
+	
+}
+int main()
+{
+	int n,m,i,j;
+	cin>>n>>m;
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<m;j++)
+		{
+			cin>>a[i][j];
+		}
+	}
+	
+}
